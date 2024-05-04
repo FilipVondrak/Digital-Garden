15fb7a848e699c08d1268de0bc49d728857803cf
Filip Vondrák <90453814+FilipVondrak@users.noreply.github.com>
2024-05-04 23:18:55 +0200

    přidání analytics

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ const tocPlugin = require("eleventy-plugin-nesting-toc");
 const { parse } = require("node-html-parser");
 const htmlMinifier = require("html-minifier");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+import { inject } from "@vercel/analytics"
+inject()
 
 const { headerToId, namedHeadingsFilter } = require("./src/helpers/utils");
 const {
