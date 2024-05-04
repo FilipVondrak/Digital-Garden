0172c05c61f88ba74a4894c57b017eeb836ecab4
Filip Vondrák <90453814+FilipVondrak@users.noreply.github.com>
2024-05-04 23:22:35 +0200

    odebrání analytics

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,5 +1,3 @@
-import { inject } from "@vercel/analytics"
-
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
@@ -7,7 +5,6 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
-    inject()
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
